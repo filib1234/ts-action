ac2f2421233a97251ab62ea98aec0f75738f57e5
filip.nawrot <filip.nawrot@antologic.com>
2022-04-04 14:57:47 +0200

    asddd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import { getInput } from "@actions/core"
+
+const inputName: string = getInput("name")
+
+greet(inputName)
+
+function greet(name: string) {
+    console.log(`'Hello ${name}!'`)
+}
+
