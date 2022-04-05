99ef6c1e398cad0f087e80da7cbc4c8300a5ad04
filip.nawrot <filip.nawrot@antologic.com>
2022-04-05 08:08:56 +0200

    get diff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { getInput } from "@actions/core"
-import { context } from "@actions/github"
+import { context, getOctokit } from "@actions/github"
+import { getDiffieHellman } from "crypto"
+import dedent from "dedent"
 
 type GithubContext = typeof context
 
 const inputName: string = getInput("name")
+const ghToken: string = getInput("ghToken")
 
 greet(inputName, getRepoUrl(context))
+
+getDiff().then(files => {
+    console.log(dedent(`
+    Your PR added
+    ${JSON.stringify(files, undefined, 2)}
+    `))
+})
 
 function greet(name: string, repoUrl: string) {
     console.log(`'Hello ${name}! ${repoUrl}'`)
@@ -13,4 +23,20 @@ function greet(name: string, repoUrl: string) {
 
 function getRepoUrl({ repo, serverUrl }: GithubContext): string {
     return `${serverUrl}/${repo.owner}/${repo.repo}`
+}
+
+async function getDiff() {
+    if (ghToken) {
+        const octokit = getOctokit(ghToken)
+
+        const result = await octokit.rest.repos.compareCommits({
+            repo: context.repo.repo,
+            owner: context.repo.owner,
+            head: context.payload.pull_request.head.sha,
+            base: context.payload.pull_request.base.sha,
+            per_page: 100
+        })
+        return result.data.files || []
+    }
+    return []
 }
